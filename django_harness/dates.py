9c972d555a75af4fc2627b087fe32d495f5f780a
Chris Wilson <chris+github@qwirx.com>
2014-12-03 16:45:24 +0000

    Add a from_now() method to DateUtilsMixin.
    Has the ability to remove microseconds and generate aware or naive datetimes,
    both useful for tests that interact with the database or ElasticSearch.

--- django_harness/dates.py
+++ django_harness/dates.py
@@ -1,10 +1,20 @@
 from __future__ import unicode_literals, absolute_import
 
-from datetime import date, timedelta
+from datetime import date, datetime, timedelta
+from django.utils.timezone import now
 
 class DateUtilsMixin(object):
     def from_today(self, **kwargs):
         return date.today() + timedelta(**kwargs)
+
+    def from_now(self, **kwargs):
+        naive = kwargs.pop('naive', False)
+        remove_ms = kwargs.pop('remove_ms', False)
+        time_now = datetime.now() if naive else now()
+        time_then = time_now + timedelta(**kwargs)
+        if remove_ms:
+            time_then -= timedelta(microseconds=time_then.microsecond)
+        return time_then
 
     def format_date(self, date, date_format=None):
         from django.template.defaultfilters import date as format_date
